1114b79c3d95f94f5e85496a1d03a9f05d8303fd
Eviix <daniel.ristikari@gmail.com>
2022-09-12 04:30:02 +0300

    valmis

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, FlatList } from 'react-native';
 
 export default function App() {
+  const [text, setText] = useState('');
+  const [data, setData] = useState([]);
+
+  const add = () => {
+    setData([...data, { key: text }]);
+    setText('');
+  }
+
+  const clear = () => {
+    setData([])
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput style={styles.input} onChangeText={setText} value={text} />
+      <View style={styles.buttons}>
+        <Button onPress={add} title="Add item" />
+        <Button onPress={clear} title="Clear item" />
+
+      </View>
+      <Text>Shopping list</Text>
+      <FlatList
+        data={data}
+        renderItem={({ item }) =>
+          <Text>{item.key}</Text>} />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +39,20 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  buttons: {
+    width: 100,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginRight: 75,
+    marginBottom: 50,
+  },
+  input: {
+    marginTop: 300,
+    marginBottom: 5,
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 1
   },
 });
